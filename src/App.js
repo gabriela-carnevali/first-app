b72ab479fb947b53ac18e9246af19fa1f4fed184
gabriela-carnevali <gabriela.c.lima10@aluno.senai.br>
2026-03-06 08:19:44 -0300

    Realização de exercícios

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import { StyleSheet, Text, View } from 'react-native';
-import { ExpressoesJSX } from './jsx_examples/02_expressoes_jsx';
-import Exemplo_01 from './jsx_examples/01_estrutura_jsx';
-import Exemplo_03 from './jsx_examples/03_condicionais_jsx';
-import Exemplo_04 from './jsx_examples/04_listas_map_jsx';
+import { StyleSheet, View } from 'react-native';
+import Lista01 from './jsx_examples/exercicios/Lista01';
+
+
+
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Exemplo_04/>
+      <Lista02/>
     </View>
   );
 }
